47b3724d5f86961f0f9782112114ee043653d7dd
vageesh <vageeshbeliya4638@gmail.com>
2026-08-06 23:22:36 +0530

    Added data driven login test and screenshot handling

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,21 +1,28 @@
+import pytest
 from pages.login_page import Loginpage
 from playwright.sync_api import expect
 import json
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(filename="logs/execution.log",level=logging.INFO,format="%(asctime)s - %(levelname)s - %(message)s")
+logging.info("FRAMEWORK STARTED")
 with open("data/login_data.json") as file:
     users = json.load(file)
-def test_login(page):
-    user = users[0]
+@pytest.mark.parametrize("user",users)
+def test_login(page,user):
     login_page = Loginpage(page)
     logging.info("We are in Login page")
     try:
+        logging.info("Attempting Login")
         login_page.login(user["username"],user["password"])
         expect(page).to_have_url('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
-        logging.indo("Login Successfull")
+        page.screenshot(path="screenshots/login_success.png",full_page = True)
+        logging.info("Login Successfull")
     except Exception as e:
         print(f"Exception is {e}")
-        page.screenshot(path="screenshots/login_page.png")
+        page.screenshot(path="screenshots/login_failed.png",full_page = True)
+        logging.info("Login Failed")
+        raise
+        
     
 
