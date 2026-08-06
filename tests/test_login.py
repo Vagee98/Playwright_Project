eae8906b7f0ae1eb5058e237164f12b0f3b8993e
vageesh <vageeshbeliya4638@gmail.com>
2026-08-06 20:01:44 +0530

    Added logging and screenshot support

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,16 +1,21 @@
 from pages.login_page import Loginpage
 from playwright.sync_api import expect
 import json
+import logging
 
+logging.basicConfig(level=logging.INFO)
 with open("data/login_data.json") as file:
     users = json.load(file)
 def test_login(page):
     user = users[0]
     login_page = Loginpage(page)
-    login_page.login(user["username"],user["password"])
-    # try:
-    # expect(page.get_by_text("Dashboard")).to_be_visible()
-    expect(page).to_have_url('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
-    # except Exception as e:
-    #     print(e)
+    logging.info("We are in Login page")
+    try:
+        login_page.login(user["username"],user["password"])
+        expect(page).to_have_url('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+        logging.indo("Login Successfull")
+    except Exception as e:
+        print(f"Exception is {e}")
+        page.screenshot(path="screenshots/login_page.png")
+    
 
